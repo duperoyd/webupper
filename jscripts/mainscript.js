b0263870673c30d335af4d2ad680c75d79b015b2
duperweb <duperweb@gmail.com>
2017-11-01 21:51:56 -0700

    something in kreyol.

---
+++ jscripts/mainscript.js
@@ -0,0 +1,7 @@
+window.addEventListener("resize", changeMade)
+
+function changeMade() {
+    if (window.innerWidth < 1000 || window.innerHeight < 800) {
+    alert("wait for the mobile version");
+}
+}
